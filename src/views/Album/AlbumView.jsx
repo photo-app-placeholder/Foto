3d53c9d451d13ba113f3d776acc73e2ca802687e
ckaynatividad <natividad.ckay@gmail.com>
2022-03-01 12:09:48 -0800

    album view renders, image view started

--- src/views/Album/AlbumView.jsx
+++ src/views/Album/AlbumView.jsx
@@ -1,21 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import profileHook from '../../hooks/profileHook';
 import { findAlbumById } from '../../services/albums';
-import { findPhotoById } from '../../services/photos';
+import { fetchPhotosByAlbumId, findPhotoById } from '../../services/photos';
 
 export default function AlbumView() {
   const [currentAlbum, setCurrentAlbum] = useState({});
   const { album } = useParams();
+  const [photos, setPhotos] = useState([]);
+  const { profile } = profileHook();
+  const { username } = profile[0];
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await findAlbumById(album);
       setCurrentAlbum(data);
       console.log(data);
-      const data2 = a;
+      const data2 = await fetchPhotosByAlbumId(album);
+      setPhotos(data2);
+      console.log(data2);
     };
     fetchData();
   }, [album]);
 
-  return <div></div>;
+  return (
+    <div>
+      {photos.map((photo) => (
+        <Link to={`/${username}/${album}/${photo.id}`} key={photo.id}>
+          <img src={photo.photo} />
+        </Link>
+      ))}
+    </div>
+  );
 }
--- src/views/Album/ImageView.jsx
+++ src/views/Album/ImageView.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function ImageView() {
+  const [photo, setPhoto] = useState({});
+
   return <div>ImageView</div>;
 }
